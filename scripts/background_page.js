8657cdd1e7e53b8e27b168b4636081629d3327a5
Tobias Tangemann <tobiastangemann@gmail.com>
2013-03-06 23:05:33 +0100

    Backup-Cache ersetzt

--- scripts/background_page.js
+++ scripts/background_page.js
@@ -1,20 +1,14 @@
 /* Const
  * ===== */
-
-var cacheMirrors = ['http://pastebin.com/raw.php?i=tVLCAjZc',
-                    'http://g1plus.x10.mx/cache.json'];
-
+var API_URL = 'http://gameone.de/blog/'
 var informationPage = 'http://g1plus.x10.mx/blog/information/';
-
 var backup_cache_url = chrome.extension.getURL("/data/backup_cache.json");
-var online_cache = false;
 
-/**
- * Altersfreigabe Daten laden die mit der Erweiterung als Backup/Fallback kommen
- * Werden direkt aus der Date geladen da der lokale Storage der von einer Extension
- * verändert werden kann auf 512 Einträge mit je 2KB begrenzt ist.
- * http://code.google.com/chrome/extensions/trunk/storage.html
- */
+var backup_cache;
+
+/* Methoden
+ * ======== */
+
 function loadBackupCache()
 {
   var xhr = new XMLHttpRequest();
@@ -22,17 +16,11 @@ function loadBackupCache()
   xhr.onreadystatechange = function()
   {
     if (xhr.readyState == 4) {
-      if(localStorage.cache == undefined) {
-        localStorage.cache = xhr.responseText;
-      }
-      else {
-        // test if valid data present
-        try {
-          JSON.parse(localStorage.cache);
-        } catch (e) {
-          // no valid json in local cache override with backup
-          localStorage.cache = xhr.responseText;
-        }
+      // test if valid data present
+      try {
+          backup_cache = JSON.parse(xhr.responseText);
+      } catch (e) {
+        console.log('Error parsing backup cache');
       }
     }
   };
@@ -40,69 +28,18 @@ function loadBackupCache()
   try {
     xhr.send();
   } catch(e) {
-    console.log('Couldn\'t load backup_cache');
+    console.log('Could not load backup_cache');
   }
 }
 
-
-/* Methoden
- * ======== */
-
-function loadOnlineCache()
-{
-  online_cache = JSON.parse(localStorage.cache);
+function getVersion() {
+  var details = chrome.app.getDetails();
+  return details.version;
 }
 
 
-/**
- * Synchronsieren des lokalen cache mit der letzten Version
- * auf pastebin oder eines Fallback-Mirrors, wenn neuer
- */
-function updateCacheFromWeb(mirror) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", cacheMirrors[mirror], true);
-  xhr.onerror = function() {
-    if(cacheMirrors[mirror + 1]) {
-        updateCache(mirror + 1);
-      }
-  }
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == this.DONE) {
-      try {
-        var json = JSON.parse(xhr.responseText);
-        if(localStorage.cache == undefined) {
-          localStorage.cache = xhr.responseText;
-          loadOnlineCache();
-        }
-        else {
-          var cache_old = JSON.parse(localStorage.cache);
-          if (cache_old['update'] && Date.parse(json['update']) > Date.parse(cache_old['update'])) {
-            localStorage.cache = xhr.responseText;
-          }
-          loadOnlineCache();
-        }
-      } catch(e) {
-        console.log('Couldn\'t update online_cache');
-      }
-    }
-  }
-  xhr.send();
-}
-
-
-function storeData(id, data) {
-  localStorage[id] = data;
-}
-
-function getData(id) {
-  // try to load data from online_cache
-  if (online_cache[id] == undefined) {
-    return 0; // TODO try to update cache here?
-  } else {
-    var a = online_cache[id];
-    return online_cache[id];
-  }
-}
+/* Event-Listener
+ * ============== */
 
 function onInstall() {
   // showInformation
@@ -114,10 +51,8 @@ function onUpdate() {
   // TODO
 }
 
-function getVersion() {
-  var details = chrome.app.getDetails();
-  return details.version;
-}
+/* Main
+ * ==== */
 
 // Check if the version has changed.
 var currVersion = getVersion();
@@ -133,13 +68,13 @@ if (currVersion != prevVersion) {
 }
 
 loadBackupCache();
-updateCacheFromWeb(0);
 
-/* Message-Request Listener
- * ======================== */
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    if (request.func == "getData") {
-      sendResponse({data: getData(request.id)});
-    }
-});
+// Modify headers for all json-api requests
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    console.log('request: ' + details);
+    return {requestHeaders: [{'name': 'User-Agent', 'value': 'GameOne'}, {'name': 'X-G1APP-IDENTIFIER', 'value': 'x'}]};
+  },
+  {urls: [API_URL + "*.json"]},
+  ["blocking", "requestHeaders"]
+);
